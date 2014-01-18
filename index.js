900fad8eb101410f4d51e57424bb227032148353
Gianfranco Cecconi <gianfranco.cecconi@digitalcontraptionsimaginarium.co.uk>
2014-01-18 11:11:02 +0000

    Early database code

--- index.js
+++ index.js
@@ -10,8 +10,30 @@ var qs = (function(a) {
     return b;
 })(window.location.search.substr(1).split('&'));
 
-var readProductInfo = function (productId, callback) {
-	d3.json("./database/products/" + productId.format + "/" + productId.contents + ".json", function (json) {
-		callback(json);
-	})
+var readDatabase = function (callback) {
+    var tells = [ ];
+    database = { };
+    // for the time being, the database is just made of the 'tells'
+	d3.json("./data/tells.json", function (tempTells) {
+        tells = tempTells.reduce(function (memo, tell) {
+            memo[tell.name] = tell;
+            return memo;
+        }, { });
+        async.each(Object.keys(tells), function (tellName, callback) {
+            d3.json("./data/tells/" + tellName + ".json", function (tellData) {
+                tells[tellName].products = tellData.products;
+                callback(!tellData.products);
+            });
+        }, function (err) {
+            database = { tells: tells };
+            console.log(JSON.stringify(database));
+            callback(err);
+        });
+    });
 }
+
+var readProductTells = function (barcode, callback) {
+    callback(null, { });
+}
+
+var database = { };
